a308a0c349b117187136353386b06a5ebd8d3922
Leeonardoo <leeonardo.deo@gmail.com>
2021-07-02 20:36:50 -0300

    Ajustes no padding dos itens da tela de multimídia

--- src/screens/MultimediaScreen.js
+++ src/screens/MultimediaScreen.js
@@ -7,32 +7,32 @@ export default function MultimediaScreen() {
             <div className="content">
                 <div id="youtube-item">
                     <iframe width="854"
-                        height="480"
-                        src="https://www.youtube.com/embed/Es8QSe48H3Y"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen />
+                            height="480"
+                            src="https://www.youtube.com/embed/Es8QSe48H3Y"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen/>
                 </div>
-                <hr />
+                <hr/>
                 <div className="mid-content">
                     <div id="video-item">
                         <video width="400" controls>
-                            <source src="/media/sample-mp4-file.mp4" type="video/mp4" />
+                            <source src="/media/sample-mp4-file.mp4" type="video/mp4"/>
                             Seu navegador não suporta a tag video.
                         </video>
                     </div>
 
                     <div id="audio-item">
                         <audio controls>
-                            <source src="/media/file_example_MP3_700KB.mp3" type="audio/mp3" />
+                            <source src="/media/file_example_MP3_700KB.mp3" type="audio/mp3"/>
                             Seu navegador não suporta a tag audio.
                         </audio>
                     </div>
                 </div>
-                <hr />
+                <hr/>
                 <div id="pdf-item">
-                    <iframe title="PDF" src="/media/sample.pdf" width="800" height="500" />
+                    <iframe title="PDF" src="/media/sample.pdf" width="800" height="500"/>
                 </div>
             </div>
         </div>
